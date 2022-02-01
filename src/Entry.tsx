0040fd3af4a5fb5dea0dc77ec1c2b4a22f0a6adb
Spring CY Wong <spring.cy.wong@prudential.com.hk>
2022-02-02 01:30:48 +0800

    added HomeScreen to replace default

---
+++ src/Entry.tsx
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
