82a7a31cb2ac29288a7d0a2c87b21716b71dab84
Spring CY Wong <spring.cy.wong@prudential.com.hk>
2022-02-02 01:41:56 +0800

    added screen transition

--- src/Entry.tsx
+++ src/Entry.tsx
@@ -2,14 +2,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import HomeScreen from "./screens/HomeScreen";
+import DefaultScreen from "./App";
 
 const Stack = createNativeStackNavigator();
+
+export type RootStackParamList = {
+    Home: undefined;
+    Default: undefined;
+};
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Default" component={DefaultScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function HomeScreen() {
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from "../Entry";
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+export default function HomeScreen({ route, navigation }: Props) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
+            <TouchableOpacity style={{
+                borderWidth: 1,
+                padding: 8,
+                borderRadius: 8,
+                marginTop: 8,
+            }} onPress={
+                () => {
+                    navigation.navigate("Default")
+                }
+            }>
+                <Text>To Default Screen</Text>
+            </TouchableOpacity>
         </View>
     );
 }
